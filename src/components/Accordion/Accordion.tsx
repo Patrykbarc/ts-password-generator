48a670390b077e6a7332c76217ce865bfaf85989
Patryk Barć <patrykbarc.design@gmail.com>
2024-03-20 21:05:47 +0100

    Accordion component

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,27 +1,54 @@
-// import { accordionData } from "../../constants/accordionData";
+import { Fragment, useState } from "react";
+import { accordionData } from "../../constants/accordionData";
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
 export function Accordion() {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  function handleSectionFold(index: number) {
+    setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
+  }
+
   return (
     <>
-      <h2 className="mt-16 text-h2 font-bold">Frequently asked questions</h2>
+      <h2 className="mb-8 mt-16 text-h2 font-bold">
+        Frequently asked questions
+      </h2>
 
-      <div className="flex flex-col gap-5 border-2">
-        <div className="flex cursor-pointer justify-between">
-          <h4 className="text-h4 font-semibold">
-            Why sould I use a password generator?
-          </h4>
-          <PlusCircleIcon className="h-12 text-neutral-700" />
-        </div>
+      {accordionData.map((data, index) => {
+        const isOpen = openIndex === index;
 
-        <p className="text-regular">
-          Computers can quickly and easily guess passwords. Any hacker using a
-          traditional desktop computer would have the opportunity to test
-          billions of different passwords in several seconds. We've developed an
-          online password generator to help you keep your private information
-          secure. Our free password generator relies on mathematical randomness
-          from an algorithm to create truly secure, random passwords.
-        </p>
-      </div>
+        return (
+          <Fragment key={data.title}>
+            <div className="flex flex-col overflow-hidden border-t-2 py-3 last-of-type:border-b-2">
+              <div
+                className="flex cursor-pointer justify-between "
+                onClick={() => handleSectionFold(index)}
+              >
+                <h4 className="text-h4 font-semibold">{data.title}</h4>
+                <PlusCircleIcon
+                  className={`h-12 text-neutral-700 transition-transform duration-500 ${isOpen ? "rotate-45 fill-indigo-400" : ""}`}
+                />
+              </div>
+
+              <div
+                style={
+                  isOpen
+                    ? {
+                        maxHeight: "10rem",
+                        transition: "max-height 300ms 300ms ease-out",
+                      }
+                    : {
+                        maxHeight: "0rem",
+                        transition: "max-height 300ms ease-out",
+                      }
+                }
+              >
+                <p className="pb-5 pt-3 text-regular">{data.description}</p>
+              </div>
+            </div>
+          </Fragment>
+        );
+      })}
     </>
   );
 }
